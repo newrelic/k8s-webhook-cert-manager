cb44bcedc8102308941335a89b6e0ce7cdb40045
Antonio Dias <adias@newrelic.com>
2020-11-27 15:27:54 +0000

    feat(ci): move to github actions (#35)
    * feat(ci): move to github actions

---
+++ RELEASE.md
@@ -0,0 +1,7 @@
+# Releasing
+
+To release this project, go into Github [releases](https://github.com/arvdias/k8s-webhook-cert-manager/releases) and "Draft a new release".
+
+Use the next tag version targeting master and use the same value for the "Release title".
+
+In the description, add the same list of changes of the CHANGELOG.md.
--- e2e-tests/tests.sh
+++ e2e-tests/tests.sh
@@ -8,6 +8,49 @@ JOB_NAME="e2e-test-cert-manager-setup"
 # shellcheck disable=SC1090
 . "$(dirname "$0")/k8s-e2e-bootstraping.sh"
 
+get_pod_name_by_label() {
+    pod_name=""
+    i=1
+    while [ "$i" -ne 10 ]
+    do
+        pod_name=$(kubectl get pods -l "$1" -o name | sed 's/pod\///g; s/pods\///g')
+        if [ "$pod_name" != "" ]; then
+            break
+        fi
+        sleep 1
+        i=$((i + 1))
+    done
+    printf "%s" "$pod_name"
+}
+
+wait_for_pod() {
+    set +e
+    desired_status=${2:-'Running'}
+    is_pod_in_desired_status=false
+    i=1
+    while [ "$i" -ne 30 ]
+    do
+        pod_status="$(kubectl get pod "$1" -o jsonpath='{.status.phase}')"
+        if [ "$pod_status" = "$desired_status" ]; then
+            is_pod_in_desired_status=true
+            printf "pod %s is %s\n" "$1" "$desired_status"
+            break
+        fi
+
+        printf "Waiting for pod %s to be %s\n" "$1" "$desired_status"
+        sleep 3
+        i=$((i + 1))
+    done
+    if [ $is_pod_in_desired_status = "false" ]; then
+        printf "pod %s does not transition to %s within 1 minute 30 seconds\n" "$1" "$desired_status"
+        kubectl get pods
+        kubectl describe pod "$1"
+        kubectl logs "$1"
+        exit 1
+    fi
+    set -e
+}
+
 finish() {
     printf "calling cleanup function\n"
     kubectl delete -f manifests/ || true
@@ -16,13 +59,14 @@ finish() {
 # build container
 (
     cd ..
-    make build-container
+    make build-container-e2e
     cd -
 )
 
 trap finish EXIT
 
 kubectl create -f manifests/webhook.yaml
+# add imagePullPolicy=Never in the manifest.
 awk '/image: / { print; print "        imagePullPolicy: Never"; next }1' manifests/cert-manager-job.yaml | kubectl create -f -
 
 label="job-name=${JOB_NAME}"
